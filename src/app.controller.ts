18d4ba3ecb85c7c00c3b33fc12354c66081e5136
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2024-05-10 02:04:03 -0300

    teste de ambiente

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -35,7 +35,7 @@ export class AppController {
     }[];
 
     return {
-      message: `[${new Date().toISOString()}] - [${process.env.INSTANCE_ID}] - O servidor está em execução`,
+      message: `[${new Date().toISOString()}] - [${process.env.INSTANCE_ID}] - O servidor está em execução - Homologação!`,
       signatures: countSignatures,
       database_info: {
         active: +countActive.toString(),
--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle(title)
-    .setDescription('Bem-vindo à API do Territory Manager de Homologação! ')
+    .setDescription('Bem-vindo à API do Territory Manager!')
     .setDescription(description)
     .setVersion(version)
     .addBearerAuth()
