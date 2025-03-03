8241e5be6fdbced032973bcb7a44fd84e828892b
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2025-03-03 16:12:03 -0300

    feat(dashboard): enhance findMarkedHouses and territoryDetails to dynamically include all types in the SQL query results

--- src/modules/dashboard/dashboard.service.ts
+++ src/modules/dashboard/dashboard.service.ts
@@ -8,37 +8,44 @@ export class DashboardService {
   async findMarkedHouses(tenantId: number) {
     const oneYearAgo = new Date();
     oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
-    const data = await this.prisma.$queryRaw<any>`
-    SELECT
-        TO_CHAR(r.completed_date, 'YYYY-MM-01') AS date,
-        CAST(SUM(CASE WHEN t.name = 'Residencial' THEN 1 ELSE 0 END) AS INT) AS residential,
-        CAST(SUM(CASE WHEN t.name = 'Comercial' THEN 1 ELSE 0 END) AS INT) AS commercial
-    FROM
-        round r
-    JOIN
-        house h ON r.house_id = h.id
-    JOIN
-        territory tr ON h.territory_id = tr.id
-    JOIN
-        type t ON tr.type_id = t.id
-    WHERE
-        r.completed = TRUE
-        AND r.tenant_id = ${tenantId}
-        AND r.completed_date >= ${oneYearAgo}
-    GROUP BY
-        TO_CHAR(r.completed_date, 'YYYY-MM-01')
-    ORDER BY
-        date;
-    `;
+    const oneYearAgoString = oneYearAgo.toISOString();
+    const types = await this.prisma.type.findMany({ where: { tenantId: tenantId } });
+    const typeColumns = types
+      .map((type) => `CAST(SUM(CASE WHEN t.name = '${type.name}' THEN 1 ELSE 0 END) AS INT) AS "${type.name}"`) // ✅ Corrigido aqui
+      .join(',\n ');
+
+    const data = await this.prisma.$queryRawUnsafe<any>(`
+      SELECT
+          TO_CHAR(r.completed_date, 'YYYY-MM-01') AS date,
+          ${typeColumns}
+      FROM
+          round r
+      JOIN
+          house h ON r.house_id = h.id
+      JOIN
+          territory tr ON h.territory_id = tr.id
+      JOIN
+          type t ON tr.type_id = t.id
+      WHERE
+          r.completed = TRUE
+          AND r.tenant_id = ${tenantId}
+          AND r.completed_date >= '${oneYearAgoString}'
+      GROUP BY
+          TO_CHAR(r.completed_date, 'YYYY-MM-01')
+      ORDER BY
+          date;
+`);
     return data;
   }
 
   async territoryDetails(tenantId: number) {
-    const data = await this.prisma.$queryRaw<any>`
+    const types = await this.prisma.type.findMany({ where: { tenantId: tenantId } });
+    const typeColumns = types
+      .map((type) => `CAST(SUM(CASE WHEN t.name = '${type.name}' THEN 1 ELSE 0 END) AS INT) AS "${type.name}"`) // ✅ Corrigido aqui
+      .join(',\n ');
+    const data = await this.prisma.$queryRawUnsafe<any>(`
     SELECT
-      CAST(SUM(CASE WHEN t.name = 'Residencial' THEN 1 ELSE 0 END) AS INT) AS residential,
-      CAST(SUM(CASE WHEN t.name = 'Comercial' THEN 1 ELSE 0 END) AS INT) AS commercial,
-      CAST(SUM(CASE WHEN t.name IN ('Interfone', 'Predial-Interno', 'Predial-Externo') THEN 1 ELSE 0 END) AS INT) AS building,
+      ${typeColumns},
       CAST(SUM(1) AS INT) AS total
     FROM
       territory tr
@@ -48,7 +55,7 @@ export class DashboardService {
       house h ON tr.id = h.territory_id
     WHERE
       h.tenant_id = ${tenantId}
-    `;
+    `);
     return data[0];
   }
 }
