cffbba0a8b24380691bf085a5f4cd6462400451a
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2025-06-29 21:46:43 -0300

    fix(records): add distinct to SQL query to prevent duplicate entries and include update date filters for accuracy

--- src/modules/records/records.service.ts
+++ src/modules/records/records.service.ts
@@ -17,7 +17,7 @@ export class RecordsService {
   constructor(private prismaService: PrismaService) { }
   async find(tenant_id: number, dateFrom: string, dateTo: string) {
     return this.prismaService.$queryRaw<RecordsData[]>`
-    select
+    select distinct 
       t.name as "Território",
       t2."name" as "Tipo Território",
       to2.overseer as "Dirigente",
@@ -27,13 +27,23 @@ export class RecordsService {
       territory_overseer to2
     inner join territory t on
       t.id = to2.territory_id
-    inner join "type" t2 on t2.id = t.type_id 
+    inner join "type" t2 on
+      t2.id = t.type_id
+    inner join round r on
+      r.territory_id = to2.territory_id
+      and r.tenant_id = to2.tenant_id
     where
       to2.tenant_id = ${tenant_id}
       and to2.initial_date >= ${dayjs(dateFrom).toDate()}
       and to2.initial_date <= ${dayjs(dateTo).toDate()}
+      and r.update_date is not null
+      and r.update_date >= to2.initial_date
+      and (
+        to2.expiration_date is null or
+        r.update_date <= to2.expiration_date
+      )
     order by
-      round_number desc,
+      to2.round_number desc,
       t.name asc,
       to2.initial_date desc
     `;
